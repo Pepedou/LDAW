8bfb95a990011b87a09ec9e29b869aa6570d1a91
Estefanía Torres Zetina <estef@estef-Inspiron-N5110>
2014-02-19 21:13:39 -0600

    Crea Clase Conexión

---
+++ Conection.php
@@ -0,0 +1,71 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Conenction
+ *
+ * @author estef
+ */
+class Conection {
+    var $host="localhost";
+    var $username="username";    // specify the sever details for mysql
+    Var $password="password";
+    var $database="database name";
+    var $myconn;
+
+    function connectToDatabase() // create a function for connect database
+    {
+
+        $conn= mysql_connect($this->host,$this->username,$this->password);
+
+        if(!$conn)// testing the connection
+        {
+            die ("Cannot connect to the database");
+        }
+
+        else
+        {
+
+            $this->myconn = $conn;
+
+            echo "Connection established";
+
+        }
+
+        return $this->myconn;
+
+    }
+
+    function selectDatabase() // selecting the database.
+    {
+        mysql_select_db($this->database);  //use php inbuild functions for select database
+
+        if(mysql_error()) // if error occured display the error message
+        {
+
+            echo "Cannot find the database ".$this->database;
+
+        }
+         echo "Database selected..";       
+    }
+
+    function closeConnection() // close the connection
+    {
+        mysql_close($this->myconn);
+
+        echo "Connection closed";
+    }
+
+}
+
+?>
+
+
+
+
+    
