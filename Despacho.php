05dd2ce68542b80d6dfc79ca2b65b9283cbc52ba
José Luis Valencia Herrera <pepedou@gmail.com>
2014-02-19 21:09:16 -0600

    Se agrega la clase Despacho.

---
+++ Despacho.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Despacho
+ *
+ * @author José Luis Valencia Herrera     A01015544
+ */
+class Despacho {
+    private $id, $nombre, $apellidoP, $apellidoM, $telefono, $email, $usuario, $pwd, $idRol, $idDespacho;
+    
+}
