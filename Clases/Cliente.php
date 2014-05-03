e7c72bf6849465574ae0a5649ed3b9450f3c0aaf
Estefanía Torres Zetina <eatz_an@hotmail.com>
2014-05-03 16:31:29 -0500

    Función get_Expedientes

--- Clases/Cliente.php
+++ Clases/Cliente.php
@@ -5,8 +5,9 @@
  *
  * @author José Luis Valencia Herrera     A01015544
  */
-include_once './EntidadBD.php';
-include_once './Direccion.php';
+include_once './Clases/EntidadBD.php';
+include_once './Clases/Direccion.php';
+include_once './Caso.php';
 
 class Cliente extends EntidadBD {
 
@@ -23,6 +24,7 @@ class Cliente extends EntidadBD {
             "id_Direccion" => -1,
             "telefono" => 0,
             "email" => "NULL",
+            "contrasena" => "",
             "visible" => 1);
         $this->discr = "email";
         $this->discrValor = $this->atributos[$this->discr];
@@ -41,12 +43,92 @@ class Cliente extends EntidadBD {
         return $direccion;
     }
 
+    public function guardarDatos(array $misDatos) {
+        foreach ($this->atributos as $campo => $valor) {
+            if ($campo === "contrasena") {
+                $this->atributos[$campo] = sha1($misDatos[$campo]);
+            } else {
+                $this->atributos[$campo] = $misDatos[$campo];
+            }
+        }
+    }
+
+    /* Funcion para generar servicio con los pagos correspondientes al cliente */
+
+    public function cargarPagos(array $datos, $callback) {
+        $id = $datos['id'];
+        $json = array();
+        $query = "SELECT * FROM " . Pago::getNombreTabla() . " WHERE id_Cliente = " . $id;
+        $resultado = $this->dbExecute($query);
+        /* Genero el JSON con los resultados */
+        if ($resultado != false) {
+            while ($fila = $resultado->fetch_assoc()) {
+                array_push($json, $fila);
+            }
+            $finalData = array("Resultados" => $json);
+            if ($callback != "") {
+                $json = "$callback(" . json_encode($finalData) . ")";
+            } else {
+                $json = json_encode($finalData);
+            }
+            print_r($json);
+        }
+        return $json;
+    }
+    
+      /* Funcion para generar servicio con los pagos correspondientes al cliente */
+
+    public function servicioCasos(array $datos, $callback) {
+        $id = $datos['id'];
+        $json = array();
+        $query = "SELECT * FROM " . Caso::getNombreTabla() . " WHERE " . Caso::getNombreTabla() . ".id_Cliente =" . $datos['id'];
+        $resultado = $this->dbExecute($query);
+        /* Genero el JSON con los resultados */
+        if ($resultado != false) {
+            while ($fila = $resultado->fetch_assoc()) {
+                array_push($json, $fila);
+            }
+            $finalData = array("Resultados" => $json);
+            if ($callback != "") {
+                $json = "$callback(" . json_encode($finalData) . ")";
+            } else {
+                $json = json_encode($finalData);
+            }
+            print_r($json);
+        }
+        return $json;
+    }
+
+    public function verificaLogin(array $datos, $callback) {
+        $email = $datos['email'];
+        $pwd = $datos['contrasena']; //necesitamos añadir contraseña al cliente
+        $data = array();
+        $query = "SELECT email FROM " . static::$tabla_static . " WHERE email = '$email' LIMIT 1";
+        $resultado = $this->dbExecute($query);
+        if ($resultado != false) {
+            $row = $resultado->fetch_assoc();
+            $data[] = array("email" => $row['email']);
+        } else {
+            $data[] = array("email" => "NULL");
+        }
+        $finalData = array("Resultados" => $data);
+        if ($callback != "") {
+            $json = "$callback(" . json_encode($finalData) . ")";
+        } else {
+            $json = json_encode($finalData);
+        }
+        print_r($json);
+    }
+
     public function generarFormaActualizacion($seleccion, $nombre, $accion, $carpeta) {
         $name = "Selecciona";
         $apellido_p = "Apellido Paterno";
         $apellido_m = "Apellido Materno";
         $telefono = "Tel.";
         $email = "Email";
+        $contrasena = "";
+        $sel_mun = 0;
+        $sel_edo = 0;
 
 
         if ($nombre !== "Selecciona") {
@@ -72,6 +154,11 @@ class Cliente extends EntidadBD {
                     $no_ext = $dir->atributos["no_exterior"];
                     $no_int = $dir->atributos["no_interior"];
                     $cd = $dir->atributos["ciudad"];
+                    $sel_mun = $dir->atributos["id_Municipio"];
+                    /* Regreso nombre de municipio */
+                    $mun = $dir->getMunicipio($sel_mun);
+                    /* Uso el nombre para traer el id del edo */
+                    $sel_edo = $dir->getIDEstadoDeMunicipio($mun);
                 }
             }
         }
@@ -88,6 +175,8 @@ class Cliente extends EntidadBD {
         static::$smarty->assign('cliente_cd', $cd);
         static::$smarty->assign('cliente_int', $no_int);
         static::$smarty->assign('cliente_ext', $no_ext);
+        static::$smarty->assign('sel_mun', $sel_mun);
+        static::$smarty->assign('sel_edo', $sel_edo);
 
         static::$smarty->assign('sel', $seleccion);
         static::$smarty->assign('name', "clientes");
@@ -117,7 +206,13 @@ class Cliente extends EntidadBD {
                 $dir = new Direccion();
                 foreach ($this->atributos as $campo => $valor) {
 
-                    $this->atributos[$campo] = $_REQUEST[$campo];
+                    if (isset($_REQUEST[$campo])) {
+                        if ($campo === "contrasena") {
+                            $this->atributos[$campo] = sha1($_REQUEST[$campo]);
+                        } else {
+                            $this->atributos[$campo] = $_REQUEST[$campo];
+                        }
+                    }
                 }
 
                 foreach ($dir->atributos as $campo => $valor) {
@@ -136,13 +231,13 @@ class Cliente extends EntidadBD {
                 $this->procesa_bajas();
                 break;
             case 3:
-                  if ($_REQUEST['sel'] !== 0) {
+                if ($_REQUEST['sel'] !== 0) {
                     $dir = new Direccion();
                     foreach ($this->atributos as $campo => $valor) {
 
-                        $this->atributos[$campo] = $_REQUEST[$campo]; 
+                        $this->atributos[$campo] = $_REQUEST[$campo];
                     }
-                        $this->atributos["visible"] = 1;
+                    $this->atributos["visible"] = 1;
                     foreach ($dir->atributos as $campo => $valor) {
                         $dir->atributos[$campo] = $_REQUEST[$campo]; //guarda los atributos para la direccion              
                     }
@@ -201,6 +296,24 @@ class Cliente extends EntidadBD {
         } else {
             return -1;
         }
+    }
+
+    public function cargarCasos() {
+        $casos = array();
+
+        $query = "SELECT * FROM " . Caso::getNombreTabla() . " WHERE " . Caso::getNombreTabla() . ".id_Cliente =" . $this->atributos['id'];
+
+        $resultado = $this->dbExecute($query);
+
+        if ($resultado->num_rows) {
+            while ($fila = $resultado->fetch_assoc()) {
+                $aux = new Caso();
+                $aux->guardarDatos($fila);
+                array_push($casos, $aux);
+            }
+        }
+
+        return $casos;
     }
 
     public static function getNombreTabla() {
